ab438b80d37bac274f762f145e10662ae889181a
Sebanti2003 <dasguptasebanti2003@gmail.com>
2023-12-09 19:24:35 +0530

    changed the limit

--- app/action.ts
+++ app/action.ts
@@ -1,6 +1,6 @@
 "use server"
 export const fetchanime=async(page:number)=>{
-    const response=await fetch(`https://shikimori.one/api/animes?page=${page}&limit=10&order=popularity`);
+    const response=await fetch(`https://shikimori.one/api/animes?page=${page}&limit=6&order=popularity`);
     const data=await response.json();
     console.log(data);
     
